4290f5a459410cf936543890913af7a8bbb1eb9d
Nejdet TUT <143900798+nejdettut@users.noreply.github.com>
2026-02-24 15:48:33 +0300

    Update badge.tsx

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
-
+import { routing } from '@/navigation';
 import { cn } from "@/lib/utils"
 
 const badgeVariants = cva(
