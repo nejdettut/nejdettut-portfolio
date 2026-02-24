56e9c632dce6bba89e63e2461a04832361044d2e
Nejdet TUT <143900798+nejdettut@users.noreply.github.com>
2026-02-24 15:52:24 +0300

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-
+import { routing } from '@/navigation';
 import { cn } from "@/lib/utils"
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
